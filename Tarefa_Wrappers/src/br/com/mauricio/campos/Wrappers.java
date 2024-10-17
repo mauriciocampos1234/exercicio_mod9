76c0a2694d518f32a9fec558b2d483094ac4f1fa
mauriciocampos1234 <156216039+mauriciocampos1234@users.noreply.github.com>
2024-10-17 14:23:37 -0300

    Mod 9

---
+++ Tarefa_Wrappers/src/br/com/mauricio/campos/Wrappers.java
@@ -0,0 +1,17 @@
+/**
+ * @author MauricioCampos
+ */
+package br.com.mauricio.campos;
+
+public class Wrappers {
+    public static void main(String[] args) {
+
+        long num = Long.valueOf(12);
+        System.out.println(num);
+    }
+}
+
+
+
+
+
